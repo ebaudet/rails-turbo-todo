364b80103370c7a964f0c1e5a0b4eb2059f8d597
Emilien Baudet <emilien.baudet@gmail.com>
2022-05-05 17:09:28 +0200

    CRUD todo as a turbo_stream app

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema[7.0].define(version: 2022_05_05_131357) do
   create_table "todos", force: :cascade do |t|
     t.string "title"
-    t.integer "status"
+    t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
